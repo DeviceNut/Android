11ebcbb84f7d972ef27b86507b7435050c669a1e
Greg de Valois <gdv@devicenut.com>
2017-11-19 16:45:45 -0800

    Tweaked button layouts.

--- app/src/main/java/com/devicenut/pixelnutctrl/Master.java
+++ app/src/main/java/com/devicenut/pixelnutctrl/Master.java
@@ -8,7 +8,6 @@ import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
