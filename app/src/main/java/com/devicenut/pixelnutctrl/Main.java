87300375a80251cb00f773a5c131525577b0f892
Greg <greg.devalois@hotmail.com>
2018-01-24 17:00:06 -0800

    Removed unnecessary effect layer for Ferris Wheel.

--- app/src/main/java/com/devicenut/pixelnutctrl/Main.java
+++ app/src/main/java/com/devicenut/pixelnutctrl/Main.java
@@ -218,7 +218,7 @@ public class Main extends Application
                     "E50 B65 W80 H50 D10 Q3 T E20 B90 C25 D30 F0 O3 T6 E20 U0 B90 H30 C45 D30 F0 I T E120 F1 I G",
                     "E20 W25 C25 D30 Q7 I T E101 F100 T E20 U0 W25 C25 D20 Q7 I T E101 F200 T G",
                     "E40 C25 D20 Q4 T E111 A0 E40 U0 V1 H270 C5 D30 Q1 I E131 F1000 O5 T5 G",
-                    "E30 C20 D60 Q7 T E160 I E120 I E111 F O3 T7 G",
+                    "E30 C20 D60 Q7 T E160 I E111 F O3 T7 G",
                     "E52 W65 C25 D60 Q1 T E150 I E120 F1000 I G",
                     "E51 C10 D60 Q4 T E112 T E131 F1 I T G",
                     "E0 B80 D10 Q3 T E111 F O10 T10 E142 F250 I T G",
