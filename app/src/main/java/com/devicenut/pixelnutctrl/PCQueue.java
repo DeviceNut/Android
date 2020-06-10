171fa3127f1abb184ae548e529f8ca793749a498
Greg <greg.devalois@hotmail.com>
2020-06-10 09:39:38 -0700

    Fix queue synchronization: caused occasional stall during configuration.

--- app/src/main/java/com/devicenut/pixelnutctrl/PCQueue.java
+++ app/src/main/java/com/devicenut/pixelnutctrl/PCQueue.java
@@ -12,10 +12,16 @@ class PCQueue<E>
     {
         qcount = count+1; // one slot still empty when queue is full
         queue = new Object[qcount];
-        head = tail = 0;
+        clear();
     }
 
-    boolean put(E e)
+    boolean empty()
+    {
+        return head==tail;
+    }
+    void clear() { head = tail = 0; }
+
+    synchronized boolean put(E e)
     {
         if (e == null) return false;
 
@@ -27,22 +33,17 @@ class PCQueue<E>
         return true;
     }
 
-    boolean empty()
-    {
-        return head==tail;
-    }
-    void clear() { head=tail; }
-
-    E peek()
+    synchronized E peek()
     {
         if (empty()) return null;
         return (E) queue[head];
     }
 
-    E get()
+    synchronized E get()
     {
-        E e = peek();
-        if (e != null) head = (head + 1) % qcount;
+        if (empty()) return null;
+        E e = (E) queue[head];
+        head = (head + 1) % qcount;
         return e;
     }
 }
